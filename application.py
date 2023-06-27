40f845e92a8398af5724661ff8c8b6558d9c59a5
jija <mochalov616@gmail.com>
2023-06-27 19:46:29 +0300

    pics generation

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageFont, ImageDraw
 import text_comparer as tc
+import difflib
 
 
 def text_to_image(text, n):
@@ -12,8 +13,28 @@ def text_to_image(text, n):
     img.save('pic/pic' + str(n) + '.jpg')
     
 
+def similar_strings(path1, path2, n):
+    txcp = tc.TextComparer(path1, path2)
+    lines = txcp.lines()
+    l1 = lines[0]
+    l2 = lines[1]
+    misses = []
+    for i in range(txcp.lenght()[0]):
+        for j in range(txcp.lenght()[1]):
+            if l1[i] != '' and l2[j] != '':
+                misses.append((i, l1[i], j, l2[j], txcp.line_by_line_similitarity(i, j)))
+    misses.sort(key=lambda obj: obj[4])
+    return misses[-1:-n-1:-1]
 
-stri = "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkl\n" \
-       "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkl\n" \
-       "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkl"
-text_to_image(stri, 1)
+def pics_gen(path1, path2):
+    misses = similar_strings(path1, path2, 30)
+    im1txt = ''
+    im2txt = ''
+    for line in misses:
+        im1txt = im1txt + 'line' + str(line[0]) + ':  ' + line[1] + '\n'
+        im2txt = im2txt + 'line' + str(line[2]) + ':  ' + line[3] + '\n'
+    text_to_image(im1txt, 1)
+    text_to_image(im2txt, 2)
+
+
+pics_gen('application.py', 'text_comparer.py')
--- text_comparer.py
+++ text_comparer.py
@@ -1,5 +1,6 @@
 import difflib
 
+
 class TextComparer:
 
     def __init__(self, path1, path2):
@@ -7,6 +8,7 @@ class TextComparer:
         file2 = open(path2)
         self.text1 = file1.read()
         self.text2 = file2.read()
+
     def similarity(self):
         normalized1 = self.text1.lower()
         normalized2 = self.text2.lower()
@@ -14,7 +16,19 @@ class TextComparer:
         return matcher.ratio()
 
     def line_by_line_similitarity(self, ind1, ind2):
-        normalized1 = self.text1[ind1].lower()
-        normalized2 = self.text2[ind2].lower()
+        lines1 = self.text1.split("\n")
+        lines2 = self.text2.split("\n")
+        normalized1 = lines1[ind1].lower()
+        normalized2 = lines2[ind2].lower()
         matcher = difflib.SequenceMatcher(None, normalized1, normalized2)
         return matcher.ratio()
+
+    def lenght(self):
+        lines1 = self.text1.split("\n")
+        lines2 = self.text2.split("\n")
+        return len(lines1), len(lines2)
+
+    def lines(self):
+        lines1 = self.text1.split("\n")
+        lines2 = self.text2.split("\n")
+        return lines1, lines2
