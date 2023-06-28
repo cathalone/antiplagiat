d8b1a835227c6da33233a6f864d5fbbb4cc1a3b7
Ivan Apanasevich <ivanco20041205@gmail.com>
2023-06-28 17:41:00 +0300

    test files

---
+++ test2.py
@@ -0,0 +1,64 @@
+class BankAccount:
+
+    def __init__(self, account_number, holder_name, balance=0):
+        self.account_numberasdfsadfsa = account_number
+        self.holder_nameasdfsdfsaf = holder_name
+        self.balanceadfsafsaf = balance
+
+    def depositasdadsad(self, amount):
+        self.balanceNOTOPJOMo += amount
+        print(f"Deposited {amount} units. New balance: {self.balance} units.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
